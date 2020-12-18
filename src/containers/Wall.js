5330f2cd5a906cd1957d78ef5d6b23f65e8ceacd
jacobsomer <jsomer@email.wm.edu>
2020-12-18 00:05:21 -0500

    Fixed Fading bug for wall.js

--- src/containers/Wall.js
+++ src/containers/Wall.js
@@ -31,120 +31,54 @@ class Wall extends React.Component {
       time:null,
       current:0,
       origin: null,
-      c1:false,
-      c2:false,
+      c1:true,
+      c2:true,
       c3:true,
       c4:true,
-      c5:true,
-      c6:true,
-      c7:true,
-      h:window.innerHeight,
-      w:window.innerWidth
+     
+    
 
     }
 
   }
+  
 
 
   visibilityManager(){
-    this.setState((state) => {
-      // Important: read `state` instead of `this.state` when updating.
-      return {w:window.innerWidth}
-    });
-    this.setState((state) => {
-      // Important: read `state` instead of `this.state` when updating.
-      return {h:window.innerHeight}
-    });
+  
     this.setState((state) => {
       // Important: read `state` instead of `this.state` when updating.
       return {current: state.current + 1}
     });
-    if (this.state.current%7===1){
+    if (this.props.theme.position>((this.props.theme.height*1)/10)){
       this.setState((state) => {
         // Important: read `state` instead of `this.state` when updating.
         return {c1: false}
       });
     }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c1: true}
-      });
-    }
-    if (this.state.current%7===2){
+  
+    if (this.props.theme.position>((this.props.theme.height*3)/10)){
       this.setState((state) => {
         // Important: read `state` instead of `this.state` when updating.
         return {c2: false}
       });
     }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c2: true}
-      });
-    }
-    if (this.state.current%7===3){
+    
+    if (this.props.theme.position>((this.props.theme.height*5)/10)){
       this.setState((state) => {
         // Important: read `state` instead of `this.state` when updating.
         return {c3: false}
       });
     }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c3: true}
-      });
-    }
-    if (this.state.current%7===4){
+  
+    if (this.props.theme.position>((this.props.theme.height*7)/10)){
       this.setState((state) => {
         // Important: read `state` instead of `this.state` when updating.
         return {c4: false}
       });
     }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c4: true}
-      });
-    }
-    if (this.state.current%7===5){
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c5: false}
-      });
-    }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c5: true}
-      });
-    }
-    if (this.state.current%7===6){
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c6: false}
-      });
-    }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c6: true}
-      });
-    }
-    if (this.state.current%7===0){
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c7: false}
-      });
-    }
-    else {
-      this.setState((state) => {
-        // Important: read `state` instead of `this.state` when updating.
-        return {c7: true}
-      });
-    }
+    
   }
-
    
    
 
@@ -155,7 +89,7 @@ class Wall extends React.Component {
     const d =new Date();
     this.setState({origin:d.getTime()})
     self.time = setInterval(function() {
-      // self.visibilityManager();
+      self.visibilityManager();
       var d = new Date();
       var seconds = (Math.round(d.getTime() / 1000));
       self.setState({time:(self.state.current.toString() +' --' +self.state.origin+'---'+self.props.theme.width+'---'+self.props.theme.height)});
@@ -170,22 +104,50 @@ class Wall extends React.Component {
 
   
       return (
-        <div className="Home" style={{flexDirection: 'row',position:'relative',backgroundImage:this.props.theme.theme.gradient,height:"100vh",width:'100%',top:'0px'}}>
+        <div className="Home" style={{flexDirection: 'row',position:'relative',backgroundImage:this.props.theme.theme.gradient,height:"200vh",width:'100%',top:'0px'}}>
           
-          
-          <CardColumns className="mx-auto" style={{maxWidth:'80vw'}}>
+          <>
+          <style type="text/css">
+            {`
+              .center {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100%;
+               
+               
+                
+              
+            }`}
+          </style>
+          <div className="center" style={{}}>
+        
+     
+          <CardColumns className="mx-auto" style={{position:'relative',maxWidth:'80vw'}}>
             
            
-            
+          <Fade in={!(this.state.c1)}>
+            <div >
             <NewsCard info={this.props.theme}/>
+            </div>
+          </Fade>
+            
             
             
              
               <div style={{width:'25vw'}}>
-            <Stat info={this.props.theme}/>  
+              <Fade in={!(this.state.c2)}>
+            <div >
+            <Stat info={this.props.theme}/>
+            </div>
+          </Fade>
             </div>
               <div style={{position:'relative',width:'25vw'}}>
-              <Reccomendation info={this.props.theme}/> 
+              <Fade in={!(this.state.c3)}>
+                <div >
+                  <Reccomendation info={this.props.theme}/>
+                </div>
+            </Fade>
             
             </div>  
   
@@ -194,33 +156,63 @@ class Wall extends React.Component {
           
             <div style={{height:''}}>
             <div style={{width:'25vw'}}>
-            <Stat info={this.props.theme}/>  
+            <Fade in={!(this.state.c1)}>
+            <div >
+            <Stat info={this.props.theme}/>
+            </div>
+          </Fade>
             </div>
             <div style={{width:'25vw'}}>
-              <Reccomendation info={this.props.theme}/>    
+            <Fade in={!(this.state.c2)}>
+                <div >
+                  <Reccomendation info={this.props.theme}/>
+                </div>
+            </Fade>
             </div>
             </div>   
             <div style={{width:'25vw'}}>
-            <Stat info={this.props.theme}/>  
+            <Fade in={!(this.state.c3)}>
+            <div >
+            <Stat info={this.props.theme}/>
+            </div>
+          </Fade>
             </div>
             <div style={{width:'25vw'}}>
-              <Reccomendation info={this.props.theme}/>    
+            <Fade in={!(this.state.c4)}>
+                <div >
+                  <Reccomendation info={this.props.theme}/>
+                </div>
+            </Fade>
             </div>
             <div style={{width:'25vw'}}>
-              <Reccomendation info={this.props.theme}/>    
+            <Fade in={!(this.state.c1)}>
+                <div >
+                  <Reccomendation info={this.props.theme}/>
+                </div>
+            </Fade>
             </div>
             
             <div style={{}}>
-              <NewsCard  info={this.props.theme}/>    
+              <Fade in={!(this.state.c2)}>
+                <div >
+                  <NewsCard info={this.props.theme}/>
+                </div>
+            </Fade>
             </div>
             <div style={{width:'25vw'}}>
-            <Stat info={this.props.theme}/>  
+            <Fade in={!(this.state.c3)}>
+                <div >
+                  <Stat info={this.props.theme}/>
+                </div>
+            </Fade>
             </div>
            
           </CardColumns>
           
+          </div>
+          </>
           
-         
+      
         </div>
       );
     }
